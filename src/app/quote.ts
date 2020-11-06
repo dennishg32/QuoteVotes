80185705a4fe5ef8cca0caf60dff49c4a4488f77
Dennis HG <dennishg32@gmail.com>
2020-11-06 23:48:20 +0200

    created a constructor that holds variables and make it public for the future use

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,13 @@
 export class Quote {
+  showDescription: boolean;
+  constructor(
+    public id: number,
+    public quote: string,
+    public author: string,
+    public createdDate: Date,
+    public upvote: number,
+    public downvote: number
+  ) {
+    this.showDescription = false;
+  }
 }
