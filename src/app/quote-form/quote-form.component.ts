ff1113da0f71c96a16bc9345e5c4b766ec78bd92
Dennis HG <dennishg32@gmail.com>
2020-11-08 19:16:06 +0200

    creation of input field

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -4,20 +4,17 @@ import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
-
-  newQuote = new Quote(0, "", "", "", new Date(), 0, 0);
+  newQuote = new Quote(0, '', '', '', new Date(), 0, 0);
   @Output() addQuote = new EventEmitter<Quote>();
 
   submitQuote() {
     this.addQuote.emit(this.newQuote);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
