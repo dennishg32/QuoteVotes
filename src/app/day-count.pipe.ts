715fdf00e5e1465460f649b29328ab05adb7af3c
Dennis HG <dennishg32@gmail.com>
2020-11-09 00:17:00 +0200

    addition of background and pipes

--- src/app/day-count.pipe.ts
+++ src/app/day-count.pipe.ts
@@ -1,12 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dayCount'
+  name: 'dayCount',
 })
 export class DayCountPipe implements PipeTransform {
+  transform(value: any): number {
+    let today: Date = new Date();
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCounter = Math.round(dateDifferenceSeconds / secondsInDay);
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    if (dateCounter >= 1 && value < todayWithNoTime) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
-
 }
